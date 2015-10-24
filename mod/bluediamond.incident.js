f30d92a97844aca58a2b9426332ed66fec52fd92
Thierry D <thierry.dtm@gmail.com>
2015-10-24 10:28:37 -0300

    Proper code structure

--- mod/bluediamond.incident.js
+++ mod/bluediamond.incident.js
@@ -4,30 +4,34 @@
  * @author Thierry D. @t_dtm
  */
 var BD = BD || {};
-BD.incident = {};
-BD.incident.instance = {};
+BD.incident = {
 
-/**
- * Stores the incident and loads it in memory as object
- * @param json
- */
-BD.incident.init = function(json)
-{
-    if (true) //todo: validate json contents stuff in json or something
+    instance: {},
+
+    /**
+     * Stores the incident and loads it in memory as object
+     * @param json
+     */
+    init: function(json)
     {
-        BD.u.log("Reading incident...");
-        BD.incident.instance = JSON.parse(json);
-        sessionStorage.setItem("incident", json);
-        localStorage.setItem("lastIncidentId", BD.incident.get("in"));
-    }
-    else
-        BD.u.log("Not a valid incident.");
-};
+        // Valid id exemple: b1b622a1-9e19-4730-98fc-6e15ac92388e
+        if (BD.u.guid.validate(json["IncidentID"])) //todo: validate json contents stuff in json or something
+        {
+            BD.u.log("GUID valid, reading incident...");
+            console.log(json);
+            this.instance = json;
+            sessionStorage.setItem("incident", JSON.stringify(json));
+            localStorage.setItem("lastIncidentId", this.get("IncidentID"));
+        }
+        else
+            BD.u.log("Not a valid incident.");
+    },
 
-BD.incident.get = function(key)
-{
-    if (BD.incident.instance.hasOwnProperty(key))
-        return BD.incident.instance[key];
-    else
-        return null;
+    get: function(key)
+    {
+        if (this.instance.hasOwnProperty(key))
+            return this.instance[key];
+        else
+            return null;
+    }
 };
