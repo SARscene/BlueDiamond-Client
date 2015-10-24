9592284ffbf821a4e2d5f47f2b6a02139bb28d5b
Thierry D <thierry.dtm@gmail.com>
2015-10-24 01:25:40 -0300

    Initial commit for server connection & lookup

---
+++ app/bluediamond.connection.js
@@ -0,0 +1,95 @@
+/**
+ * Connection
+ * @date 2015-10-24
+ * @author Thierry D. @t_dtm
+ */
+var BD = BD || {};
+BD.connection = {};
+
+/**
+ * Looks for active connections from known servers.
+ * TODO: Trigger: make cleaner and trigger connectionSucceed event instead
+ *
+ * @returns {boolean} true if a valid connection is found
+ */
+BD.connection.lookup = function()
+{
+    var i = 0;
+    var validServer = false;
+    var servers = BD.connection.list();
+
+    while (!validServer && i in servers)
+    {
+        validServer = BD.connection.handShake(servers[i]);
+        i++;
+    }
+
+    if (validServer){
+        sessionStorage.setItem("connection", url);
+        return BD.connection.fetch();
+    }
+    else
+    {
+        BD.u.log("No available collection, requesting QR code"); //TODO: request QR code
+        return false;
+    }
+};
+
+/**
+ * Checks if there is a valid BlueDiamond Server at the provided URL
+ *
+ * @param url
+ */
+BD.connection.handShake = function(url)
+{
+    $.get("//"+url, function(e){
+        BD.u.log("Connection SUCCESS for " + url);
+        return url;
+    }).done(function() {
+        BD.u.log("Connection DONE for " + url);
+    })
+    .fail(function() {
+        BD.u.log("Connection FAILED for " + url);
+        return false;
+    });
+};
+
+/**
+ * Retrives list of previously contacted servers.
+ * @returns {Array} of URLs
+ */
+BD.connection.list = function()
+{
+    return JSON.parse(localStorage.getItem("servers")) || [];
+};
+
+/**
+ * Adds an URL to
+ * @param url
+ */
+BD.connection.add = function(url)
+{
+    var list  = BD.connection.list() || [];
+    list.push(url);
+    localStorage.setItem("servers",JSON.stringify(list));
+};
+
+/**
+ * Gets data
+ */
+BD.connection.fetch = function()
+{
+    var url = sessionStorage.getItem("connection");
+    var entryPoint = ""; //TODO: set entrypoint URL
+
+    $.get("//" + url, function(e){
+        BD.u.log("Fetch SUCCESS");
+        return url;
+    }).done(function() {
+        BD.u.log("Fetch DONE");
+    })
+    .fail(function() {
+        BD.u.log("Fetch FAILED");
+        return false;
+    });
+};
---
+++ app/bluediamond.utilities.js
@@ -0,0 +1,25 @@
+/**
+ * Utilites
+ * @date 2015-10-24
+ * @author Thierry D. @t_dtm
+ */
+var BD = BD || {};
+BD.u = {};
+
+/***
+ * Validates the field based on HTML5 form validation. TODO: do it for real.
+ * @param (field) HTML input
+ * @param (callback) must accept 1 argument = field value
+ * @returns {boolean|?} or callback's return value
+ */
+BD.u.validate = function(field, callback)
+{
+    //TODO: validate URL format
+    if (field.val().length > 0) return callback(field.val());
+    else return false;
+};
+
+BD.u.log = function(msg)
+{
+    if (window.console) console.log("["+(new Date().getTime())+"] " + msg);
+};
